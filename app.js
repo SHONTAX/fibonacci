cac709cc990213e1a3d6ee67c488289ed1e05630
SHONTAX <shontax.mail@gmail.com>
2021-08-03 14:07:38 +0900

    メモによるアルゴリズム改善

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 'use strict';
-function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+function fib(n){
+    if (memo.has(n)){ // n=3
+        return memo.get(n);
+    } //           fib(2) + fib(1) => fib(1) fib(0) + fib(1) => 1 + 0 + 1
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 
 const length = 40;
-for (let i = 0; i <= length; i++) {
-    console.log(fib(i))
+for (let i = 0; i <= length; i++){
+    console.log(fib(i));
 }
